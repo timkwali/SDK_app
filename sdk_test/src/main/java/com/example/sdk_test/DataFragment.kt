62bcfb9801476b4f97a8fa4e909e9598733ee9fb
timkwali <timkwalit@accessbankplc.com>
2022-08-03 15:38:31 +0100

    refactor code

--- sdk_test/src/main/java/com/example/sdk_test/DataFragment.kt
+++ sdk_test/src/main/java/com/example/sdk_test/DataFragment.kt
@@ -10,25 +10,24 @@ import android.widget.Button
 import android.widget.Toast
 
 
-class DataFragment {
+class DataFragment : Fragment() {
 
 
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.fragment_data, container, false)
-//
-//        val btn: Button? = view?.findViewById(R.id.btn)
-////        btn?.setOnClickListener {
-////            showToastMessage()
-////        }
-//        showToastMessage(requireContext(), "SDK test toast message")
-//    }
-    companion object {
-        fun showToastMessage(context: Context, message: String) {
-            Toast.makeText(context, message, Toast.LENGTH_LONG).show()
-        }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_data, container, false)
+
+        val btn: Button? = view?.findViewById(R.id.btn)
+//        btn?.setOnClickListener {
+//            showToastMessage()
+//        }
+        showToastMessage(requireContext(), "SDK test toast message")
+    }
+
+    fun showToastMessage(context: Context, message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
     }
 }
